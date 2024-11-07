8717efa20552f8dd35e45194d3c37ce86d9e9856
JC Reid <thawedbuffalo@gmail.com>
2024-11-06 21:55:12 -0500

    chapter 13 section 13

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,15 +1,17 @@
+import 'dart:io';
+
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class UserPlacesNotifer extends StateNotifier<List<Place>> {
-  UserPlacesNotifer() : super(const []);
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title) {
-    final newPlace = Place(title: title);
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
     state = [newPlace, ...state];
   }
 }
 
 final userPlacesProvider =
-    StateNotifierProvider<UserPlacesNotifer, List<Place>>(
-        (ref) => UserPlacesNotifer());
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+        (ref) => UserPlacesNotifier());
--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:favorite_places/providers/user_places.dart';
 import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
@@ -12,14 +14,15 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -47,7 +50,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ),
             const SizedBox(height: 30,),
             // Image input
-            const ImageInput(),
+            ImageInput(onPickImage: (image) {
+              _selectedImage = image;
+              },
+            ),
             const SizedBox(
               height: 16,
             ),
